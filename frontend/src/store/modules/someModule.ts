6b5049e28890580916cc8e2aca0899919f8d9b3c
P9WEB <p9web.info@gmail.com>
2022-02-24 09:48:43 -0500

    Revert "initial commit"
    This reverts commit 2092b449

---
+++ frontend/src/store/modules/someModule.ts
@@ -0,0 +1,11 @@
+const namespaced = true
+
+const state = () => ({})
+
+const getters = {}
+
+const actions = {}
+
+const mutations = {}
+
+export default { namespaced, state, getters, actions, mutations }
